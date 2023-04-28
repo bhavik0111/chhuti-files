ac98111fdf5031f8f08c7ee06ae0342cc11dc99a
bhavik0111 <118987227+bhavik0111@users.noreply.github.com>
2023-04-28 05:50:47 -0700

    Add files via upload

---
+++ views/blog/index.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.layout.master')
+@section('content')
+	<h1>Blog Posts</h1>
+
+	@foreach ($blogs as $blog)
+		<div>
+			<h2>{{ $blog->title }}</h2>
+			<p>{{ $blog->content }}</p>
+			<a href="{{ route('blog.show', ['id' => $blog->id]) }}">Read more</a>
+		</div>
+	@endforeach
+@endsection
