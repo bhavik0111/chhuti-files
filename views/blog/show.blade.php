92576287ccd65c1fc0f623f275fca051dc629f95
bhavik0111 <118987227+bhavik0111@users.noreply.github.com>
2023-04-29 08:21:43 +0530

    Delete views directory

--- views/blog/show.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- @extends('layouts.app')
-@section('content')
-	<h1>Blog views</h1> -->
-	<!-- @foreach ($blogs as $blog) -->
-		{{ $blog->title }}
-		{{ $blog->content }}
-	<!-- @endforeach -->
-<!-- @endsection -->
